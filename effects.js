218b956b843767fa304e4380f2100c63f5d4ee0c
kalyan <kalyanraja012@gmail.com>
2024-11-22 16:52:52 -0500

    Initial commit

--- effects.js
+++ effects.js
@@ -12,3 +12,37 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 }); 
+
+const cursor = {
+    init() {
+        this.dot = document.querySelector('.cursor-dot');
+        this.dotOutline = document.querySelector('.cursor-dot-outline');
+        this.setPosition({ clientX: 0, clientY: 0 });
+        this.bindEvents();
+    },
+
+    bindEvents() {
+        document.addEventListener('mousemove', (e) => {
+            requestAnimationFrame(() => this.setPosition(e));
+        });
+        document.addEventListener('mouseenter', () => this.show());
+        document.addEventListener('mouseleave', () => this.hide());
+    },
+
+    setPosition(e) {
+        this.dot.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+        this.dotOutline.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+    },
+
+    show() {
+        this.dot.style.opacity = '1';
+        this.dotOutline.style.opacity = '1';
+    },
+
+    hide() {
+        this.dot.style.opacity = '0';
+        this.dotOutline.style.opacity = '0';
+    }
+};
+
+cursor.init(); 
